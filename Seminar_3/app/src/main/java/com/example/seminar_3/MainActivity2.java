b3601dfba64dc5be2fe32af9ca944b804206c51a
SpulberAdrian <101510130+SpulberAdrian@users.noreply.github.com>
2024-10-20 23:05:04 +0300

    Add files via upload

---
+++ Seminar_3/app/src/main/java/com/example/seminar_3/MainActivity2.java
@@ -0,0 +1,37 @@
+package com.example.seminar_3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+       String s = intent.getStringExtra("TEXT");
+       Integer i1 =  intent.getIntExtra("Numar",0);
+       Integer i2 = intent.getIntExtra("Numar2",0);
+        Toast.makeText(this, s+(i1+i2), Toast.LENGTH_SHORT).show();
+
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+
+        });
+
+    }
+
+
+}
